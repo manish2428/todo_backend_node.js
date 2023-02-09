da00fedfc3127ecff438c81a83b3a6c83430fdf7
manish yadav <manishyadav2056107@gmail.com>
2023-02-09 23:44:57 +0530

    update:Resoled errors related to database in login and signup route

--- app.js
+++ app.js
@@ -27,29 +27,32 @@ res.send("This is homepage")
 })
 
 app.post('/login',async(req,res)=>{
-     const password=Signup.findOne({email:req.body.password})
-     
-     bcrypt.compare(req.body.password,password, (err, data) => {
-          if (err) throw err
+          const password=await Signup.find({email:req.body.email})
+          bcrypt.compare(req.body.password,password[0].password, (err, data) => {
+          if(err){
+              return res.status(400).json({"err":"error_occured"})
+          }
           if (data) {
-              return res.status(200).json({ msg: "Login success" })
-          } else {
-              return res.status(401).json({ msg: "Invalid credencial" })
+              return res.status(200).json({ "msg": "Login success" })
+          }else {
+              return res.status(401).json({ "msg": "Invalid credencial" })
           }
 
-      })
+          })
 })
 
 app.post('/signup',async(req,res)=>{
      if(!emailvalidator.validate(req.body.email)){
-          res.json({'error':"Please provide valide email address","status_code":500})
+          return res.json({'error':"Please provide valide email address","status_code":500})
      }
      const hash=await bcrypt.hashSync(req.body.password,10)
 
-
+ 
      try{
-     
-          const signup=new Signup({
+          const fetch_email=await Signup.find({email:req.body.email})
+          // console.log(fetch_email)
+          if(fetch_email.length===0){
+          const signup=await new Signup({
                fullname:req.body.fullname,
                email:req.body.email,
                password:hash,
@@ -57,13 +60,18 @@ app.post('/signup',async(req,res)=>{
                delete:req.body.delete
           })
           signup.save();
-    }catch{
-          res.json({'error':'error occured while inserting data in mongo'})
-    }finally{
-          res.json({'message':'User registered successfully',"Status_code":200})
-    }
-     
-     
+          }
+          else{
+               res.status_code=500
+               return res.json({"error":"Email already exist."})
+          }
+     }catch{
+          res.status_code=400
+          return res.json({'error':'error occured while inserting data in mongo'})
+     }finally{
+          res.status_code=200
+          return res.json({'message':'User registered successfully'})
+     }
 })
 
 app.listen(process.env.PORT||3000,process.env.IP,function(req,res){
--- routing/list.js
+++ routing/list.js
@@ -2,6 +2,7 @@ const express=require("express")
 const router=express.Router()
 
 const List=require('../Model/database_model')
+// const Signup=require('../Model/signup_models')
 
 
 router.get("/list",async(req,res)=>{
@@ -15,27 +16,56 @@ router.get("/list",async(req,res)=>{
     }
 })
 
-router.post("/list",(req,res)=>{
+router.post("/list",async(req,res)=>{
     try{
-        const list=List({
+        const list1=await List.find({title:req.body.title})
+        
+        if(list1.length===0){
+        const list=await List({
             title:req.body.title,
             description:req.body.description
         })
         list.save()
+        }
+        else{   
+         res.status_code=500
+         return res.json({"message":"Title cant be same"})
+        }
     }catch{
-        return res.status(400).json({"message":"error while inserting data"})
+        res.status_code=400
+        return res.json({"message":"error while inserting data"})
     }finally{
-        return res.status(200).json({"message":"Document Inserted Successfully"})
+        res.status_code=200
+        return res.json({"message":"Document Inserted Successfully"})
     }
-    // res.status(200).json({"message":"Document Inserted Successfully"})  
+
 })
 
-router.put('/list/:id',(req,res)=>{
-    res.status(200).json({"key":"list_put"})
+router.put('/list/:id',async(req,res)=>{
+    try{
+        updated_data={$set:{"title":req.body.title,"description":req.body.description}}
+        var list=await List.updateOne({_id:req.params.id},{$set:{"title":req.body.title,"description":req.body.description}})
+        // console.log(list)
+    }catch{
+        return res.status(400).json({"message":"Error occured while updating data"})
+    }finally{
+        return res.status(200).json({"message":list})
+    }
+     
 })
 
-router.delete('/list/:id',(req,res)=>{
-    res.status(200).json({"key":"list_delete"})
+router.delete('/list/:id',async(req,res)=>{
+    try{
+        updated_data={$set:{"title":req.body.title,"description":req.body.description}}
+        var list=await List.deleteOne({_id:req.params.id})
+        // console.log(list)
+    }catch{
+        res.status_code=400
+        return res.json({"message":"Error occured while deleting document"})
+    }finally{
+        res.status_code=200
+        return res.json({"message":"deleted","data":list})
+    }
 })
 
 module.exports=router
