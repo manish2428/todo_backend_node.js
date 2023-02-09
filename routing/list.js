b6e1d00c5fc812108a0679a0fda46b7d04774207
manish yadav <manishyadav2056107@gmail.com>
2023-02-09 16:56:03 +0530

    feature:insert and get operation implemented in TODO list

--- routing/list.js
+++ routing/list.js
@@ -4,20 +4,38 @@ const router=express.Router()
 const List=require('../Model/database_model')
 
 
-router.get("/list",(req,res)=>{
-    res.json({"key":"list_get","status":200})
+router.get("/list",async(req,res)=>{
+    try{
+        var list=await List.find({})
+    }catch{
+        return res.status(400).json({"Message":"Internal server error"})
+    }finally{
+        
+       return res.status(200).json({"data":list})
+    }
 })
 
 router.post("/list",(req,res)=>{
-    res.json({"key":"list_post","status":200})
+    try{
+        const list=List({
+            title:req.body.title,
+            description:req.body.description
+        })
+        list.save()
+    }catch{
+        return res.status(400).json({"message":"error while inserting data"})
+    }finally{
+        return res.status(200).json({"message":"Document Inserted Successfully"})
+    }
+    // res.status(200).json({"message":"Document Inserted Successfully"})  
 })
 
 router.put('/list/:id',(req,res)=>{
-    res.json({"key":"list_put","status":200})
+    res.status(200).json({"key":"list_put"})
 })
 
 router.delete('/list/:id',(req,res)=>{
-    res.json({"key":"list_delete","status_code":200})
+    res.status(200).json({"key":"list_delete"})
 })
 
 module.exports=router
